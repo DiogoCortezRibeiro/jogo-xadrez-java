b5695a4cb1507f714c3ccd55aa4e88dd6801ba50
DiogoCortezRibeiro <diogocortezr@gmail.com>
2021-01-26 20:19:44 -0300

    Implements ChessPiece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		ChessMatch chess = new ChessMatch();
+		UI.printBoard(chess.getPieces());
 	}
 
 }
