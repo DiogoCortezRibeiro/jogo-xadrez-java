a1ffe5dccc7f14b2aebdc53914a43644cb65009d
DiogoCortezRibeiro <diogocortezr@gmail.com>
2021-01-26 21:01:02 -0300

    Position and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,8 +24,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BLACK), new Position(2,1));
-		board.placePiece(new King(board, Color.WHITE), new Position(2,7));
+		this.placeNewPiece('b', 6, new Rook(board, Color.BLACK));
+		this.placeNewPiece('g', 2, new King(board, Color.WHITE));
 	}
 }
